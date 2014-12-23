605672e12f5550bade5e9cb3690b023724f23331
statwonk <cpeter9@gmail.com>
2014-12-23 16:28:25 -0500

    All data cleaned and loaded.

--- load_data.R
+++ load_data.R
@@ -38,6 +38,9 @@ write.table(paste0("http://s3.amazonaws.com/fl-crash/", d[50:length(d)]), file =
 d <- list.files(".")[grepl('2011', list.files(".")) | grepl('2012', list.files(".")) | grepl('2013', list.files("."))]
 d <- d[grepl(".csv", d)]
 # d <- d[grepl("2011_2011", d)]
+d <- list.files(".")[grepl('2010', list.files(".")) &
+                       grepl('.txt', list.files("."))]
+
 
 saveRDSfast <- function (object, file = "", ascii = FALSE, version = NULL, compress = TRUE,
                          refhook = NULL) {
@@ -92,19 +95,24 @@ table(names(d[[1]]) == names(d[[2]]))
 table(names(d[[1]]) == names(d[[3]]))
 table(names(d[[1]]) == names(d[[4]]))
 table(names(d[[1]]) == names(d[[5]]))
+
 g1 <- do.call(rbind, lapply(1:5, function(x) { d[[x]] }))
+
+g2 <- d[[6]]
 # Group 2
-table(names(d[[6]]) == names(d[[7]]))
-table(names(d[[6]]) == names(d[[8]]))
-table(names(d[[6]]) == names(d[[9]]))
-g2 <- do.call(rbind, lapply(6:9, function(x) { d[[x]] }))
+table(names(d[[7]]) == names(d[[8]]))
+table(names(d[[7]]) == names(d[[9]]))
+g3 <- do.call(rbind, lapply(7:9, function(x) {
+  d[[x]]$crash_date_time <- as.character(d[[x]]$crash_date_time)
+  d[[x]]
+  }))
 
 library(dplyr)
 
-g1_clean <- g1[ , c("Crash Date", "Hour", "Minute", "AMPM"), with = F]
-setnames(g1_clean, 1:4, c("crash_date", "hour", "min", "ampm"))
-g1_clean <- tbl_df(as.data.frame(g1_clean))
-g1_clean <- g1_clean %>%
+g1 <- g1[ , c("Crash Date", "Hour", "Minute", "AMPM"), with = F]
+setnames(g1, 1:4, c("crash_date", "hour", "min", "ampm"))
+g1 <- tbl_df(as.data.frame(g1))
+g1 <- g1 %>%
   mutate(ampm = ifelse(ampm == "P", "PM", "AM")) %>%
   mutate(time = as.POSIXct(paste0(crash_date, " ",
                                   hour, ":",
@@ -115,24 +123,46 @@ g1_clean <- g1_clean %>%
   select(crash_at = time) %>%
   mutate(counter = 1)
 
-g2_clean <- g2[ , c("crash_date_time"), with = F]
-setnames(g2_clean, 1, "crash_at")
-g2_clean[, counter := 1,]
+g2 <- as.data.table(g2)
+g2 <- g2[ , c("Crash.Date", "Hour", "Minute", "AMPM"), with = F]
+setnames(g2, 1:4, c("crash_date", "hour", "min", "ampm"))
+g2 <- tbl_df(as.data.frame(g2))
+g2 <- g2 %>%
+  mutate(ampm = ifelse(ampm == "P", "PM", "AM")) %>%
+  mutate(time = as.POSIXct(paste0(crash_date, " ",
+                                  hour, ":",
+                                  min, ":00 ",
+                                  ampm),
+                           format = "%m/%d/%Y %I:%M:00 %p",
+                           tz = "EST")) %>%
+  select(crash_at = time) %>%
+  mutate(counter = 1)
 
-g1_clean <- as.data.table(as.data.frame(g1_clean))
-g2_clean <- as.data.table(as.data.frame(g2_clean))
+g3 <- as.data.table(g3)
+g3 <- g3[ , c("crash_date_time"), with = F]
+setnames(g3, 1, "crash_at")
+g3[, counter := 1,]
+g3[ , crash_at := paste0(crash_at, ":00"), ]
+g3 <- g3[nchar(crash_at) == nchar("2011-01-01 00:00:00")]
+g3[ , crash_at := as.POSIXct(crash_at,
+                          format = "%Y-%m-%d %H:%M:%S",
+                          tz = "EST", origin = "1970-01-01"), ]
 
-g3 <- rbind(g1_clean, g2_clean)
+g1 <- as.data.table(as.data.frame(g1))
+g2 <- as.data.table(as.data.frame(g2))
+g3 <- as.data.table(as.data.frame(g3))
+
+g <- rbind(g1, g2, g3)
 
 # clean up
-rm(g1, g2, g1_clean, g2_clean, d)
+rm(g1, g2, g3, d)
 
-g3[ ,crash_at := format(crash_at, "%Y-%m-%d"),]
-setkey(g3, crash_at)
-g3 <- g3[ , list(crashes = sum(counter)), by = "crash_at"]
-g3 <- g3[!is.na(crash_at) & as.integer(substr(crash_at, 1, 4)) >= 2005]
+g[ ,crash_at := format(crash_at, "%Y-%m-%d"),]
+setkey(g, crash_at)
+g <- g[ , list(crashes = sum(counter)), by = "crash_at"]
+g <- g[!is.na(crash_at) & as.integer(substr(crash_at, 1, 4)) >= 2005]
 
-input <- tbl_df(as.data.frame(g3))
+input <- tbl_df(as.data.frame(g))
 input <- input %>% mutate(crash_at = as.POSIXct(crash_at, tz = "EST"))
 # Fill in dropped dates, if any
 input <- left_join(tbl_df(data.frame(crash_at = seq(min(input$crash_at),
@@ -153,7 +183,7 @@ ggplot(input,
   ggtitle("Crashes on Florida Roads") +
   ylab("Crashes per day") +
   xlab("") +
-  scale_y_continuous(breaks = seq(0, 25, 2.5)) +
+  # scale_y_continuous(breaks = seq(0, 25, 2.5)) +
   theme(panel.grid.major.y = element_line(colour = "black", size = 1),
         panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank(),
