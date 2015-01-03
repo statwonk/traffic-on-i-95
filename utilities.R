b89cf0e68da1cdd5e4ab4df2a1caa9a291786cde
statwonk <cpeter9@gmail.com>
2015-01-03 15:32:51 -0500

    Package to analyze data and the legacy utilities file which is being folded into our new package floridaCrashR

---
+++ utilities.R
@@ -0,0 +1,35 @@
+return_raw_files <- function() {
+  data <- xmlParse("s3list.xml")
+  xml_data <- xmlToList(data)
+  d <- unlist(do.call(rbind, xml_data[-c(1:5)]))
+  d <- d[grepl(".txt", d) | grepl(".csv", d)]
+
+  return(d)
+}
+
+saveRDSfast <- function (object, file = "", ascii = FALSE,
+                         version = NULL, compress = TRUE,
+                         refhook = NULL) {
+  if (is.character(file)) {
+    if (file == "")
+      stop("'file' must be non-empty string")
+    mode <- if (ascii)
+      "w"
+    else "wb"
+    con <- if (identical(compress, "bzip2"))
+      bzfile(file, mode, compression = 1)
+    else if (identical(compress, "xz"))
+      xzfile(file, mode, compress = 1)
+    else if (compress)
+      gzfile(file, mode, compress = 1)
+    else file(file, mode)
+    on.exit(close(con))
+  }
+  else if (inherits(file, "connection")) {
+    if (!missing(compress))
+      warning("'compress' is ignored unless 'file' is a file name")
+    con <- file
+  }
+  else stop("bad 'file' argument")
+  .Internal(serializeToConn(object, con, ascii, version, refhook))
+}
